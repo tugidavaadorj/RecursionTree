f9eb027d442011a0eb250cfb0780322e8a83510e
Tugi Davaadorj <tugidavaadorj@Tugis-MacBook-Pro.local>
2020-11-14 16:36:36 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+var slider = document.getElementById("rightRange");
+var output = document.getElementById("demoRight");
+var slid = document.getElementById("leftRange");
+var out = document.getElementById("demoLeft");
+output.innerHTML = slider.value;
+var highBound = slider.value;
+var lowBound = slid.value;
+slider.oninput = function() {
+    highBound = this.value;
+    output.innerHTML = this.value;
+    draw();
+}
+
+
+
+out.innerHTML = slid.value;
+slid.oninput = function() {
+    lowBound = this.value;
+    out.innerHTML = this.value;
+    draw();
+}
+
+function draw() {
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.fillStyle='black';
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+        ctx.fillStyle='white';
+        ctx.translate(canvas.width/2, canvas.height);
+        var len = 100;
+        branch(ctx, len)
+    }
+  }
+
+function branch(ctx, len){
+    ctx.fillRect(0, 0, 1, -len);
+    ctx.translate(1, -len);
+    if (len>2){
+        //var angle = Math.floor((Math.random() * (highBound-lowBound)) + lowBound);
+        var angle = highBound;
+        ctx.save();
+        ctx.rotate(angle*Math.PI/180);
+        branch(ctx, len*.67);
+
+        
+        ctx.restore();
+        angle = lowBound;
+        //angle = Math.floor((Math.random() * (highBound-lowBound)) + lowBound);
+        ctx.rotate(-angle*Math.PI/180);
+        branch(ctx, len*.67);
+        
+    }
+}
+
